3075b7bf6092b3128ae30439ca4a073b0425bbea
R4KO <rakotovahiny@hotmai.fr>
2020-05-11 12:19:20 +0200

    Revert "Revert "Test numéro 4004""
    This reverts commit 3f15bf94cce014aab26a5213dc0a52e4c441df4c.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ceci sera le main");
-        System.out.println("un autre hello world");
+        HelloWorld.sayHelloWorld();
     }
 }
