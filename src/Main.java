2018ac1a136ca665404c8a95aeee1fb882efed8f
Léo <toto.malge@gmail.com>
2020-05-11 12:13:10 +0200

    test de léo

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("ceci sera le main");
         System.out.println("un autre hello world");
         System.out.println("vous voyez les modifs ?");
+        System.out.println("test avec kraken");
+        //je met un commentaire  ouaiiiiiiiiis
     }
 }
