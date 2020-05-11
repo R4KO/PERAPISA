94f2dc44b5f63767ff5ede06a30d86084cc4f058
florian <florian.perez@efrei.net>
2020-05-11 12:38:21 +0200

    J'aime le poulet

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void sayHelloWorld() {
         System.out.println("Hello World");
+        for(int i = 1; i < 21; i++){
+            System.out.println("J'aime le poulet et j'ai " + i + "ans");
+        }
     }
 }
