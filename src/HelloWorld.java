321b1981b19437d5703c7c72c6cf9adaa7be4a69
Léo <toto.malge@gmail.com>
2020-05-11 11:52:41 +0200

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         
         System.out.println("Hello World!");
         System.out.println("projet java test");
+        System.out.println("autre test ");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ceci sera le main");
         System.out.println("un autre hello world");
+        System.out.println("vous voyez les modifs ?");
     }
 }
