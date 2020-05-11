ef344523ad904beec739270669de066a29020892
Anselmep <65160345+Anselmep@users.noreply.github.com>
2020-05-11 11:48:52 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         
         System.out.println("Thomas a réussi a push du code");
+        System.out.println("Ans le s aussi a réussi a push du code");
         
         System.out.println("Hello World!");
         System.out.println("projet java test");
