425ac82a5d1260462df7f5e7a2124e68fdfd7955
Thomas <thomas.sauwal@efrei.net>
2020-06-10 10:31:51 +0200

    Complément phase 1 qcm
    Pour la phase Qcm on peut récupérer les résultats a la fin en affichant un tableau des scores lorsque chaque joueur a répondu à la question posée

--- src/players/Phase1Q.java
+++ src/players/Phase1Q.java
@@ -76,6 +76,8 @@ public class Phase1Q extends javax.swing.JFrame {
         
         Joueurs1.setText(j1.toString());
         jButton2.setVisible(false);
+        score.setVisible(false);
+        jButton4.setVisible(false);
         jButton3.setVisible(false);
     }
 
@@ -103,9 +105,16 @@ public class Phase1Q extends javax.swing.JFrame {
         jRadioButton2 = new javax.swing.JRadioButton();
         jRadioButton3 = new javax.swing.JRadioButton();
         jRadioButton4 = new javax.swing.JRadioButton();
+        jButton4 = new javax.swing.JButton();
+        score = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(204, 204, 255));
+
+        jPanel2.setBackground(new java.awt.Color(204, 255, 204));
+
+        jLabel1.setFont(new java.awt.Font("Microsoft Himalaya", 1, 36)); // NOI18N
         jLabel1.setText("Phase 1 : Tour 1");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
@@ -127,11 +136,13 @@ public class Phase1Q extends javax.swing.JFrame {
 
         Joueurs1.setEditable(false);
         Joueurs1.setColumns(20);
+        Joueurs1.setFont(new java.awt.Font("Microsoft Himalaya", 0, 30)); // NOI18N
         Joueurs1.setRows(5);
         jScrollPane1.setViewportView(Joueurs1);
 
         question.setEditable(false);
         question.setColumns(20);
+        question.setFont(new java.awt.Font("Microsoft Himalaya", 0, 30)); // NOI18N
         question.setRows(5);
         jScrollPane2.setViewportView(question);
 
@@ -168,30 +179,42 @@ public class Phase1Q extends javax.swing.JFrame {
         buttonGroup2.add(jRadioButton4);
         jRadioButton4.setText("jRadioButton4");
 
+        jButton4.setText("Validez");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        score.setText("Affichez les scores");
+        score.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                scoreActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(153, 153, 153)
+                .addGap(24, 24, 24)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(590, 590, 590)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(106, 106, 106)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 395, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGap(27, 27, 27)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(107, 107, 107)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jRadioButton2)
                     .addComponent(jRadioButton1)
                     .addComponent(jRadioButton3)
-                    .addComponent(jRadioButton4))
-                .addGap(109, 109, 109))
+                    .addComponent(jRadioButton4)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(16, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(score, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                     .addContainerGap(906, Short.MAX_VALUE)
@@ -202,30 +225,33 @@ public class Phase1Q extends javax.swing.JFrame {
                     .addContainerGap(919, Short.MAX_VALUE)
                     .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGap(21, 21, 21)))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addContainerGap(929, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(11, 11, 11)))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(56, 56, 56)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(63, 63, 63)
                         .addComponent(jRadioButton1)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(35, 35, 35)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(18, 18, 18)
-                                .addComponent(jRadioButton2)
-                                .addGap(18, 18, 18)
-                                .addComponent(jRadioButton3)
-                                .addGap(18, 18, 18)
-                                .addComponent(jRadioButton4))))
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(54, 54, 54)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 24, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jRadioButton2)
+                        .addGap(18, 18, 18)
+                        .addComponent(jRadioButton3)
+                        .addGap(32, 32, 32)
+                        .addComponent(jRadioButton4)
+                        .addGap(61, 61, 61)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(48, 48, 48)
+                .addComponent(score, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(210, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(jPanel1Layout.createSequentialGroup()
                     .addGap(384, 384, 384)
@@ -236,6 +262,11 @@ public class Phase1Q extends javax.swing.JFrame {
                     .addGap(395, 395, 395)
                     .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addContainerGap(395, Short.MAX_VALUE)))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addContainerGap(405, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(385, 385, 385)))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -272,45 +303,210 @@ public class Phase1Q extends javax.swing.JFrame {
              JOptionPane.showMessageDialog(null, "bravo bonne réponse tu marques 2 points");
         }
         else{
-        
+            SelectionJoueur.tab[0].afficher(); 
             JOptionPane.showMessageDialog(null, "dommage la bonne réponse était " + ini.bonReponse.get(te));
         
-        
-        
         }
-       
-       
-       
-       
-
         Joueur j2 = new Joueur();
         j2 = SelectionJoueur.tab[1];
         Joueurs1.setText(j2.toString());
+        
+        question.setText(ini.enonceQuestion.get(te));
+        
+        int ti = (int) (Math.random() * 100 % 4);
+        if (ti % 4 == 0) {
+            jRadioButton1.setText(ini.bonReponse.get(te));
+            jRadioButton2.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton3.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton4.setText(ini.autreRepQcm.get((te*3)+2));
+        }else if(ti % 4 == 1){
+            jRadioButton2.setText(ini.bonReponse.get(te));
+            jRadioButton1.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton3.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton4.setText(ini.autreRepQcm.get((te*3)+2));
+        }else if(ti % 4 == 2){
+            jRadioButton3.setText(ini.bonReponse.get(te));
+            jRadioButton2.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton1.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton4.setText(ini.autreRepQcm.get((te*3)+2));
+        }else{
+            jRadioButton4.setText(ini.bonReponse.get(te));
+            jRadioButton2.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton3.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton1.setText(ini.autreRepQcm.get((te*3)+2));
+        }
+        
+
         jButton1.setVisible(false);
+        jButton4.setVisible(false);
         jButton2.setVisible(true);
+        
+        
+        
+        
+        
+        
+        
+        
 
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+       String choix = null;
+       Enumeration elements = buttonGroup2.getElements();
+       while(elements.hasMoreElements()){
+           
+           AbstractButton button = (AbstractButton)elements.nextElement();
+           if(button.isSelected()){
+               choix = button.getText();  
+           }
+       }
+       JOptionPane.showMessageDialog(null, "a"+ini.bonReponse.get(te)+"a"+choix);
+        if(ini.bonReponse.get(te).equals(choix)){
+        
+            SelectionJoueur.tab[1].mAJScore(2);
+            SelectionJoueur.tab[1].afficher(); 
+             JOptionPane.showMessageDialog(null, "bravo bonne réponse tu marques 2 points");
+        }
+        else{
+            SelectionJoueur.tab[1].afficher(); 
+            JOptionPane.showMessageDialog(null, "dommage la bonne réponse était " + ini.bonReponse.get(te));
+        
+        }
+        
+        
+        
+        
         Joueur j3 = new Joueur();
         j3 = SelectionJoueur.tab[2];
         Joueurs1.setText(j3.toString());
         
+        question.setText(ini.enonceQuestion.get(te));
+        
+        int ti = (int) (Math.random() * 100 % 4);
+        if (ti % 4 == 0) {
+            jRadioButton1.setText(ini.bonReponse.get(te));
+            jRadioButton2.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton3.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton4.setText(ini.autreRepQcm.get((te*3)+2));
+        }else if(ti % 4 == 1){
+            jRadioButton2.setText(ini.bonReponse.get(te));
+            jRadioButton1.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton3.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton4.setText(ini.autreRepQcm.get((te*3)+2));
+        }else if(ti % 4 == 2){
+            jRadioButton3.setText(ini.bonReponse.get(te));
+            jRadioButton2.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton1.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton4.setText(ini.autreRepQcm.get((te*3)+2));
+        }else{
+            jRadioButton4.setText(ini.bonReponse.get(te));
+            jRadioButton2.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton3.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton1.setText(ini.autreRepQcm.get((te*3)+2));
+        }
         
         
         
         jButton2.setVisible(false);
+        jButton4.setVisible(false);
         jButton3.setVisible(true);
 
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        
+       String choix = null;
+       Enumeration elements = buttonGroup2.getElements();
+       while(elements.hasMoreElements()){
+           
+           AbstractButton button = (AbstractButton)elements.nextElement();
+           if(button.isSelected()){
+               choix = button.getText();  
+           }
+       }
+       JOptionPane.showMessageDialog(null, "a"+ini.bonReponse.get(te)+"a"+choix);
+        if(ini.bonReponse.get(te).equals(choix)){
+        
+            SelectionJoueur.tab[2].mAJScore(2);
+            SelectionJoueur.tab[2].afficher(); 
+             JOptionPane.showMessageDialog(null, "bravo bonne réponse tu marques 2 points");
+        }
+        else{
+            SelectionJoueur.tab[2].afficher(); 
+            JOptionPane.showMessageDialog(null, "dommage la bonne réponse était " + ini.bonReponse.get(te));
+        
+        }
+        
+        
+        
         Joueur j4 = new Joueur();
         j4 = SelectionJoueur.tab[3];
         Joueurs1.setText(j4.toString());
+        jButton3.setVisible(false);
+        jButton4.setVisible(true);
+        question.setText(ini.enonceQuestion.get(te));
+        
+        int ti = (int) (Math.random() * 100 % 4);
+        if (ti % 4 == 0) {
+            jRadioButton1.setText(ini.bonReponse.get(te));
+            jRadioButton2.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton3.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton4.setText(ini.autreRepQcm.get((te*3)+2));
+        }else if(ti % 4 == 1){
+            jRadioButton2.setText(ini.bonReponse.get(te));
+            jRadioButton1.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton3.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton4.setText(ini.autreRepQcm.get((te*3)+2));
+        }else if(ti % 4 == 2){
+            jRadioButton3.setText(ini.bonReponse.get(te));
+            jRadioButton2.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton1.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton4.setText(ini.autreRepQcm.get((te*3)+2));
+        }else{
+            jRadioButton4.setText(ini.bonReponse.get(te));
+            jRadioButton2.setText(ini.autreRepQcm.get(te*3));
+            jRadioButton3.setText(ini.autreRepQcm.get((te*3)+1));
+            jRadioButton1.setText(ini.autreRepQcm.get((te*3)+2));
+        }
+        jButton3.setVisible(false);
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        String choix = null;
+       Enumeration elements = buttonGroup2.getElements();
+       while(elements.hasMoreElements()){
+           
+           AbstractButton button = (AbstractButton)elements.nextElement();
+           if(button.isSelected()){
+               choix = button.getText();  
+           }
+       }
+       JOptionPane.showMessageDialog(null, "a"+ini.bonReponse.get(te)+"a"+choix);
+        if(ini.bonReponse.get(te).equals(choix)){
+        
+            SelectionJoueur.tab[3].mAJScore(2);
+            SelectionJoueur.tab[3].afficher(); 
+             JOptionPane.showMessageDialog(null, "bravo bonne réponse tu marques 2 points");
+        }
+        else{
+            SelectionJoueur.tab[3].afficher(); 
+            JOptionPane.showMessageDialog(null, "dommage la bonne réponse était " + ini.bonReponse.get(te));
+        
+        }
+        jButton4.setVisible(false);
+        score.setVisible(true);
+        
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void scoreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_scoreActionPerformed
+        // TODO add your handling code here:
+        new AfficherScoreTour1().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_scoreActionPerformed
 
     /**
      * @param args the command line arguments
@@ -387,6 +583,7 @@ public class Phase1Q extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
@@ -397,5 +594,6 @@ public class Phase1Q extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea question;
+    private javax.swing.JButton score;
     // End of variables declaration//GEN-END:variables
 }
